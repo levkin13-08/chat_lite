0111967ae4558a23d370fc2a1c2ef44922d83c94
Александр <102365356+levkin13-08@users.noreply.github.com>
2022-11-30 19:03:36 +0300

    Сделал стили для чата, сделал серверную часть с помощью node.js и socket

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-function JoinBlock() {
+function JoinBlock({onLogin}) {
+    const [roomId, setRoomId] = React.useState('');
+    const [userName, setUserName] = React.useState('');
+    const [isLoading, setLoading] = React.useState(false);
+
+    const onEnter = async () => {
+        if (!roomId || !userName) {
+            return (alert('Неверные данные'));
+        }
+        const obj = {
+            userName,
+            roomId,
+        };
+        setLoading(true );
+        await axios.post ('/rooms', obj);
+            onLogin(obj);
+    }
     return (    
-
         <div className = "join-block">
-            <input type="text" placeholder="Room ID"/>
-            <input type="text" placeholder="Name"/>
-            <button  className="btn btn-sucess">Войти</button>
+            <input type="text" placeholder="Room ID" 
+                value={roomId} 
+                onChange={e => setRoomId(e.target.value)}/>
+            <input type="text" placeholder="Name" 
+                value={userName}
+                onChange={e => setUserName(e.target.value)}/>
+            <button disabled = {isLoading} onClick={onEnter} className="btn btn-sucess">{isLoading ? 'Вход...' : 'Войти'}</button>
         </div>
     );
 }
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,4 @@
 import io from 'socket.io-client';
-import socket from "./socket";
 
 const socket = io();
 
